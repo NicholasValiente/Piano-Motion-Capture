2413e4e43f747be9d483165ce027e6094c6fdafe
NicholasValiente <17465742@student.uws.edu.au>
2016-03-30 10:03:51 +1100

    added test web-socket server
    test web socket server added, later to be modified to be our main
    server, but for now it moves a single point along all 3 axis

---
+++ Demo/DemoServer/DemoServer/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Fleck;
+using Newtonsoft.Json;
+
+namespace DemoServer
+{
+    class Program
+    {
+        static List<IWebSocketConnection> Sockets;
+        public class point
+        {
+             public int x , y, z;
+        }
+      
+        static void Main(string[] args)
+        {
+           
+            var p = new point() { x = 0, y = 0, z = 0 };
+           
+
+            //initialise connection
+            
+            var server = new WebSocketServer("ws://137.154.151.239:3000/relay");
+
+
+            server.ListenerSocket.NoDelay = true;
+
+
+            Console.WriteLine("Dummy server started\n");
+
+            FleckLog.Level = LogLevel.Debug;
+ 
+
+            try //currently erroring out here with invalid local endpoint
+            {
+                server.Start(socket =>
+                {
+                    
+                    socket.OnOpen = () =>
+                        {
+                            Sockets.Add(socket);
+                            Console.WriteLine("Connection Sucessful");
+                        };
+
+
+                    socket.OnClose = () =>
+                        {
+                            Sockets.Remove(socket);
+                            Console.WriteLine("Connection Terminated");
+
+                        };
+
+
+                    socket.OnMessage = message =>
+                        {
+                            Console.WriteLine(message);
+
+                        };
+                      
+                     
+
+                });
+            }
+            catch (WebSocketException e)
+            {
+                Console.WriteLine("ERROR:/n" + e.ToString() );
+            }
+
+            //after connecting
+           
+
+            while (true) //later to be changed to while connected
+            {
+                for(int i=0; i<80; i++)
+                {
+                    if (i<20 || i>=60)   
+                    { 
+                        p.x++;
+                        p.y++;
+                        p.z++;
+                    }
+                    else if (i >= 20 && i<60)
+                    {
+                        p.x--;
+                        p.y--;
+                        p.z--;
+                    }
+
+                    String message = JsonConvert.SerializeObject(p);
+
+                    Sockets.ToList().ForEach(s=> s.Send (message));
+                } //end for loop
+
+
+            } //end while loop
+
+
+        } //end main
+    }
+}

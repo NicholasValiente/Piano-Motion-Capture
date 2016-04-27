71c983e9e4ecc48108757c2a6b6459c4033387f7
NicholasValiente <17465742@student.uws.edu.au>
2016-04-27 13:53:22 +1000

    clean up on previous
    removed a bunch of un-needed or commented out lines.
    testing also shows, full keyboard must be set to octave 4 unless we want
    to expand keys array and switch case to range from 0-127

--- Working version/Midi Server/controller.js
+++ Working version/Midi Server/controller.js
@@ -57,6 +57,8 @@ function moveKey (num, dir)
 		{keys[num][1]+=2;}
 	else if (dir=="down")
 		{keys[num][1]-=2;}
+	
+	socket.send (JSON.stringify(keys) );	
 }
 
 function onMIDIFailure(error) {
@@ -68,415 +70,205 @@ function onMIDIMessage(message) {
     data = message.data; // this gives us our [command/channel, note, velocity] data.
 	switch (data[1])
 	{
-		/*
 		case 36:
-			if (data[0]==144)	{moveKey(0, "down");}
-			else 				{moveKey(0, "up");}
+			if (data[0] == 144) { moveKey(0, "down"); }
+			else { moveKey(0,"up"); }
 			break;
 		case 37:
-			if (data[0]==144)	{moveKey(1, "down");}
-			else 				{moveKey(1, "up");}
-			break;	
+			if (data[0] == 144) { moveKey(1, "down"); }
+			else { moveKey(1,"up"); }
+			break;
 		case 38:
-			if (data[0]==144)	{moveKey(2, "down");}
-			else 				{moveKey(2, "up");}
+			if (data[0] == 144) { moveKey(2, "down"); }
+			else { moveKey(2,"up"); }
 			break;
 		case 39:
-			if (data[0]==144)	{moveKey(3, "down");}
-			else 				{moveKey(3, "up");}
+			if (data[0] == 144) { moveKey(3, "down"); }
+			else { moveKey(3,"up"); }
 			break;
 		case 40:
-			if (data[0]==144)	{moveKey(4, "down");}
-			else 				{moveKey(4, "up");}
-			break;	
+			if (data[0] == 144) { moveKey(4, "down"); }
+			else { moveKey(4,"up"); }
+			break;
 		case 41:
-			if (data[0]==144)	{moveKey(5, "down");}
-			else 				{moveKey(5, "up");}
+			if (data[0] == 144) { moveKey(5, "down"); }
+			else { moveKey(5,"up"); }
 			break;
 		case 42:
-			if (data[0]==144)	{moveKey(6, "down");}
-			else 				{moveKey(6, "up");}
-			break;			
+			if (data[0] == 144) { moveKey(6, "down"); }
+			else { moveKey(6,"up"); }
+			break;
 		case 43:
-			if (data[0]==144)	{moveKey(7, "down");}
-			else 				{moveKey(7, "up");}
+			if (data[0] == 144) { moveKey(7, "down"); }
+			else { moveKey(7,"up"); }
 			break;
 		case 44:
-			if (data[0]==144)	{moveKey(8, "down");}
-			else 				{moveKey(8, "up");}
+			if (data[0] == 144) { moveKey(8, "down"); }
+			else { moveKey(8,"up"); }
 			break;
 		case 45:
-			if (data[0]==144)	{moveKey(9, "down");}
-			else 				{moveKey(9, "up");}
+			if (data[0] == 144) { moveKey(9, "down"); }
+			else { moveKey(9,"up"); }
 			break;
 		case 46:
-			if (data[0]==144)	{moveKey(10, "down");}
-			else 				{moveKey(10, "up");}
+			if (data[0] == 144) { moveKey(10, "down"); }
+			else { moveKey(10,"up"); }
 			break;
 		case 47:
-			if (data[0]==144)	{moveKey(11, "down");}
-			else 				{moveKey(11, "up");}
-			break;	
-		case 48:
-			if (data[0]==144)	{moveKey(12, "down");}
-			else 				{moveKey(12, "up");}
+			if (data[0] == 144) { moveKey(11, "down"); }
+			else { moveKey(11,"up"); }
 			break;
 		case 48:
-			if (data[0]==144)	{moveKey(13, "down");}
-			else 				{moveKey(13, "up");}
+			if (data[0] == 144) { moveKey(12, "down"); }
+			else { moveKey(12,"up"); }
 			break;
 		case 49:
-			if (data[0]==144)	{moveKey(14, "down");}
-			else 				{moveKey(14, "up");}
+			if (data[0] == 144) { moveKey(13, "down"); }
+			else { moveKey(13,"up"); }
 			break;
 		case 50:
-			if (data[0]==144)	{moveKey(15, "down");}
-			else 				{moveKey(15, "up");}
+			if (data[0] == 144) { moveKey(14, "down"); }
+			else { moveKey(14,"up"); }
 			break;
 		case 51:
-			if (data[0]==144)	{moveKey(16, "down");}
-			else 				{moveKey(16, "up");}
+			if (data[0] == 144) { moveKey(15, "down"); }
+			else { moveKey(15,"up"); }
 			break;
 		case 52:
-			if (data[0]==144)	{moveKey(17, "down");}
-			else 				{moveKey(17, "up");}
+			if (data[0] == 144) { moveKey(16, "down"); }
+			else { moveKey(16,"up"); }
 			break;
 		case 53:
-			if (data[0]==144)	{moveKey(18, "down");}
-			else 				{moveKey(18, "up");}
+			if (data[0] == 144) { moveKey(17, "down"); }
+			else { moveKey(17,"up"); }
 			break;
 		case 54:
-			if (data[0]==144)	{moveKey(19, "down");}
-			else 				{moveKey(19, "up");}
+			if (data[0] == 144) { moveKey(18, "down"); }
+			else { moveKey(18,"up"); }
 			break;
 		case 55:
-			if (data[0]==144)	{moveKey(20, "down");}
-			else 				{moveKey(20, "up");}
-			break;	
+			if (data[0] == 144) { moveKey(19, "down"); }
+			else { moveKey(19,"up"); }
+			break;
 		case 56:
-			if (data[0]==144)	{moveKey(21, "down");}
-			else 				{moveKey(21, "up");}
-			break;	
+			if (data[0] == 144) { moveKey(20, "down"); }
+			else { moveKey(20,"up"); }
+			break;
 		case 57:
-			if (data[0]==144)	{moveKey(22, "down");}
-			else 				{moveKey(22, "up");}
+			if (data[0] == 144) { moveKey(21, "down"); }
+			else { moveKey(21,"up"); }
 			break;
 		case 58:
-			if (data[0]==144)	{moveKey(23, "down");}
-			else 				{moveKey(23, "up");}
+			if (data[0] == 144) { moveKey(22, "down"); }
+			else { moveKey(22,"up"); }
 			break;
-			*/
-			
-			
-					case 36:
-		if (data[0] == 144) { moveKey(0, "down"); }
-		 else { moveKey(0,"up"); }
-		 break;
-
-
-		case 37:
-		if (data[0] == 144) { moveKey(1, "down"); }
-		 else { moveKey(1,"up"); }
-		 break;
-
-
-		case 38:
-		if (data[0] == 144) { moveKey(2, "down"); }
-		 else { moveKey(2,"up"); }
-		 break;
-
-
-		case 39:
-		if (data[0] == 144) { moveKey(3, "down"); }
-		 else { moveKey(3,"up"); }
-		 break;
-
-
-		case 40:
-		if (data[0] == 144) { moveKey(4, "down"); }
-		 else { moveKey(4,"up"); }
-		 break;
-
-
-		case 41:
-		if (data[0] == 144) { moveKey(5, "down"); }
-		 else { moveKey(5,"up"); }
-		 break;
-
-
-		case 42:
-		if (data[0] == 144) { moveKey(6, "down"); }
-		 else { moveKey(6,"up"); }
-		 break;
-
-
-		case 43:
-		if (data[0] == 144) { moveKey(7, "down"); }
-		 else { moveKey(7,"up"); }
-		 break;
-
-
-		case 44:
-		if (data[0] == 144) { moveKey(8, "down"); }
-		 else { moveKey(8,"up"); }
-		 break;
-
-
-		case 45:
-		if (data[0] == 144) { moveKey(9, "down"); }
-		 else { moveKey(9,"up"); }
-		 break;
-
-
-		case 46:
-		if (data[0] == 144) { moveKey(10, "down"); }
-		 else { moveKey(10,"up"); }
-		 break;
-
-
-		case 47:
-		if (data[0] == 144) { moveKey(11, "down"); }
-		 else { moveKey(11,"up"); }
-		 break;
-
-
-		case 48:
-		if (data[0] == 144) { moveKey(12, "down"); }
-		 else { moveKey(12,"up"); }
-		 break;
-
-
-		case 49:
-		if (data[0] == 144) { moveKey(13, "down"); }
-		 else { moveKey(13,"up"); }
-		 break;
-
-
-		case 50:
-		if (data[0] == 144) { moveKey(14, "down"); }
-		 else { moveKey(14,"up"); }
-		 break;
-
-
-		case 51:
-		if (data[0] == 144) { moveKey(15, "down"); }
-		 else { moveKey(15,"up"); }
-		 break;
-
-
-		case 52:
-		if (data[0] == 144) { moveKey(16, "down"); }
-		 else { moveKey(16,"up"); }
-		 break;
-
-
-		case 53:
-		if (data[0] == 144) { moveKey(17, "down"); }
-		 else { moveKey(17,"up"); }
-		 break;
-
-
-		case 54:
-		if (data[0] == 144) { moveKey(18, "down"); }
-		 else { moveKey(18,"up"); }
-		 break;
-
-
-		case 55:
-		if (data[0] == 144) { moveKey(19, "down"); }
-		 else { moveKey(19,"up"); }
-		 break;
-
-
-		case 56:
-		if (data[0] == 144) { moveKey(20, "down"); }
-		 else { moveKey(20,"up"); }
-		 break;
-
-
-		case 57:
-		if (data[0] == 144) { moveKey(21, "down"); }
-		 else { moveKey(21,"up"); }
-		 break;
-
-
-		case 58:
-		if (data[0] == 144) { moveKey(22, "down"); }
-		 else { moveKey(22,"up"); }
-		 break;
-
-
 		case 59:
-		if (data[0] == 144) { moveKey(23, "down"); }
-		 else { moveKey(23,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(23, "down"); }
+			else { moveKey(23,"up"); }
+			break;
 		case 60:
-		if (data[0] == 144) { moveKey(24, "down"); }
-		 else { moveKey(24,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(24, "down"); }
+			else { moveKey(24,"up"); }
+			break;
 		case 61:
-		if (data[0] == 144) { moveKey(25, "down"); }
-		 else { moveKey(25,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(25, "down"); }
+			else { moveKey(25,"up"); }
+			break;
 		case 62:
-		if (data[0] == 144) { moveKey(26, "down"); }
-		 else { moveKey(26,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(26, "down"); }
+			else { moveKey(26,"up"); }
+			break;
 		case 63:
-		if (data[0] == 144) { moveKey(27, "down"); }
-		 else { moveKey(27,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(27, "down"); }
+			else { moveKey(27,"up"); }
+			break;
 		case 64:
-		if (data[0] == 144) { moveKey(28, "down"); }
-		 else { moveKey(28,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(28, "down"); }
+			else { moveKey(28,"up"); }
+			break;
 		case 65:
-		if (data[0] == 144) { moveKey(29, "down"); }
-		 else { moveKey(29,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(29, "down"); }
+			else { moveKey(29,"up"); }
+			break;
 		case 66:
-		if (data[0] == 144) { moveKey(30, "down"); }
-		 else { moveKey(30,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(30, "down"); }
+			else { moveKey(30,"up"); }
+			break;
 		case 67:
-		if (data[0] == 144) { moveKey(31, "down"); }
-		 else { moveKey(31,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(31, "down"); }
+			else { moveKey(31,"up"); }
+			break;
 		case 68:
-		if (data[0] == 144) { moveKey(32, "down"); }
-		 else { moveKey(32,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(32, "down"); }
+			else { moveKey(32,"up"); }
+			break;
 		case 69:
-		if (data[0] == 144) { moveKey(33, "down"); }
-		 else { moveKey(33,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(33, "down"); }
+			else { moveKey(33,"up"); }
+			break;
 		case 70:
-		if (data[0] == 144) { moveKey(34, "down"); }
-		 else { moveKey(34,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(34, "down"); }
+			else { moveKey(34,"up"); }
+			break;
 		case 71:
-		if (data[0] == 144) { moveKey(35, "down"); }
-		 else { moveKey(35,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(35, "down"); }
+			else { moveKey(35,"up"); }
+			break;
 		case 72:
-		if (data[0] == 144) { moveKey(36, "down"); }
-		 else { moveKey(36,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(36, "down"); }
+			else { moveKey(36,"up"); }
+			break;
 		case 73:
-		if (data[0] == 144) { moveKey(37, "down"); }
-		 else { moveKey(37,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(37, "down"); }
+			else { moveKey(37,"up"); }
+			break;
 		case 74:
-		if (data[0] == 144) { moveKey(38, "down"); }
-		 else { moveKey(38,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(38, "down"); }
+			else { moveKey(38,"up"); }
+			break;
 		case 75:
-		if (data[0] == 144) { moveKey(39, "down"); }
-		 else { moveKey(39,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(39, "down"); }
+			else { moveKey(39,"up"); }
+			break;
 		case 76:
-		if (data[0] == 144) { moveKey(40, "down"); }
-		 else { moveKey(40,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(40, "down"); }
+			else { moveKey(40,"up"); }
+			break;
 		case 77:
-		if (data[0] == 144) { moveKey(41, "down"); }
-		 else { moveKey(41,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(41, "down"); }
+			else { moveKey(41,"up"); }
+			break;
 		case 78:
-		if (data[0] == 144) { moveKey(42, "down"); }
-		 else { moveKey(42,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(42, "down"); }
+			else { moveKey(42,"up"); }
+			break;
 		case 79:
-		if (data[0] == 144) { moveKey(43, "down"); }
-		 else { moveKey(43,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(43, "down"); }
+			else { moveKey(43,"up"); }
+			break;
 		case 80:
-		if (data[0] == 144) { moveKey(44, "down"); }
-		 else { moveKey(44,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(44, "down"); }
+			else { moveKey(44,"up"); }
+			break;
 		case 81:
-		if (data[0] == 144) { moveKey(45, "down"); }
-		 else { moveKey(45,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(45, "down"); }
+			else { moveKey(45,"up"); }
+			break;
 		case 82:
-		if (data[0] == 144) { moveKey(46, "down"); }
-		 else { moveKey(46,"up"); }
-		 break;
-
-
+			if (data[0] == 144) { moveKey(46, "down"); }
+			else { moveKey(46,"up"); }
+			break;
 		case 83:
-		if (data[0] == 144) { moveKey(47, "down"); }
-		 else { moveKey(47,"up"); }
-		 break;
-		 
+			if (data[0] == 144) { moveKey(47, "down"); }
+			else { moveKey(47,"up"); }
+			break;
 		case 84:
-		if (data[0] == 144) { moveKey(48, "down"); }
-		 else { moveKey(48,"up"); }
-		 break;
-
-
-
-
-			
+			if (data[0] == 144) { moveKey(48, "down"); }
+			else { moveKey(48,"up"); }
+			break;
 		default:
-			console.log("key not configured" + data[1]);
+			console.log("key\t" +data[1] +"\t not configured" );
 			//ect for all other keys			
 	}
-	/*
-	var textbox = document.getElementById("midiBox");
-	textbox.innerHTML += "<br/> State: " + data[0] +" Key: " + data[1] +" Velocity: " + data[2];
-	textbox.scrollTop = textbox.scrollHeight;
 	
-	//socket.send(data[0] +" " +  data[1] +" " + data[2]);
-	socket.send(JSON.stringify(data));
-	*/
-	console.log( JSON.stringify(keys) );
-	socket.send (JSON.stringify(keys) );	
-	//console.log ("sent");
 }

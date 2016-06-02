aa87b27ceffaa1ece8f70823a4d58f1b17bc3dbf
Nathaniel Pangilinan <18218015@student.westernsydney.edu.au>
2016-06-02 10:50:44 +1000

    Final Commit of the project

--- Working version/startup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-start ./websocket-relay.pl
-sleep 1000
-start  chrome localhost/MoVis_modified/MoVis.html
-sleep 100
-start  chrome "localhost/Midi Server/midiServer.html"
-sleep 100
-start  chrome "localhost/leap server/leapServer.html"
